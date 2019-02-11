d16838d399cc84316233980c8886cfd42788bbcf
Prashant Saxena <prashant.saxena@partner.bmw.de>
2019-02-11 14:11:18 +0100

    Ädded navigation drawer

--- app/src/main/java/com/android/munichapplication/MainActivity.kt
+++ app/src/main/java/com/android/munichapplication/MainActivity.kt
@@ -2,11 +2,71 @@ package com.android.munichapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mDrawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.mipmap.baseline_menu_white_18dp)
+        }
+
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // Handle navigation view item clicks here.
+            when (menuItem.itemId) {
+
+                R.id.nav_munich -> {
+                    Toast.makeText(this, "Munich", Toast.LENGTH_LONG).show()
+                }
+                R.id.nav_stuttgart -> {
+                    Toast.makeText(this, "Stuttgart", Toast.LENGTH_LONG).show()
+                }
+                R.id.nav_advertisements -> {
+                    Toast.makeText(this, "Advertisements", Toast.LENGTH_LONG).show()
+                }
+                R.id.nav_setting -> {
+                    Toast.makeText(this, "Setting", Toast.LENGTH_LONG).show()
+                }
+            }
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
+    }
+
+    //appbar - toolbar button click
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
